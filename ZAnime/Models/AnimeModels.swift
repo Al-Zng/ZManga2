017b3fff816a7cb826e2d87830488bc0d8821ff8
Al-Zng <mohammedznger@gmail.com>
2026-05-04 23:03:18 +0300

    Update AnimeModels.swift

--- ZAnime/Models/AnimeModels.swift
+++ ZAnime/Models/AnimeModels.swift
@@ -1,37 +1,45 @@
 import Foundation
 
-// MARK: - نموذج الأنمي في القائمة الرئيسية
-struct AnimeItem: Identifiable, Codable {
+// MARK: - نموذج الأنمي في القائمة
+struct AnimeItem: Identifiable, Codable, Hashable {
     var id = UUID()
     let title: String
     let url: String
     let imageURL: String
     var status: String = "مستمر"
     var type: String = "مسلسل"
+    var episodeCount: String = ""
+    var rating: String = ""
 }
 
 // MARK: - تفاصيل الأنمي
 struct AnimeDetail {
-    var imageURL: String
-    var title: String
-    var description: String = "يتم التحميل..."
+    var imageURL: String = ""
+    var title: String = ""
+    var description: String = ""
     var rating: String = "0.0"
-    var status: String = "مكتمل"
-    var type: String = "مسلسل"
+    var status: String = ""
+    var type: String = ""
     var releaseYear: String = ""
-    var studio: String = "غير معروف"
+    var studio: String = ""
+    var season: String = ""
+    var duration: String = ""
+    var genres: [String] = []
     var episodes: [Episode] = []
 }
 
 // MARK: - الحلقة
-struct Episode: Identifiable {
+struct Episode: Identifiable, Hashable {
     var id = UUID()
     var number: String
+    var title: String = ""
+    var date: String = ""
+    var url: String = ""
     var servers: [Server] = []
 }
 
 // MARK: - السيرفر
-struct Server: Identifiable {
+struct Server: Identifiable, Hashable {
     var id = UUID()
     var name: String
     var quality: String
