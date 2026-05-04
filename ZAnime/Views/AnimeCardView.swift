e656fc3fb84590b8d89ceccd60ed22a5b204594b
Al-Zng <mohammedznger@gmail.com>
2026-05-04 22:37:21 +0300

    Update AnimeCardView.swift

--- ZAnime/Views/AnimeCardView.swift
+++ ZAnime/Views/AnimeCardView.swift
@@ -5,10 +5,22 @@ struct AnimeCardView: View {
     var body: some View {
         VStack {
             AsyncImage(url: URL(string: anime.imageURL)) { phase in
-                if let image = phase.image { image.resizable().aspectRatio(contentMode: .fill) }
-                else { Rectangle().fill(Color.gray) }
-            }.frame(width: 110, height: 150).cornerRadius(8).clipped()
-            Text(anime.title).font(.caption).bold().foregroundColor(.white).lineLimit(2).multilineTextAlignment(.center)
-        }.frame(width: 110)
+                if let image = phase.image {
+                    image.resizable().aspectRatio(contentMode: .fill)
+                } else {
+                    Rectangle().fill(Color.gray)
+                }
+            }
+            .frame(width: 110, height: 160)
+            .cornerRadius(8)
+            .clipped()
+            
+            Text(anime.title)
+                .font(.caption)
+                .foregroundColor(.white)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+        }
+        .frame(width: 110)
     }
 }
